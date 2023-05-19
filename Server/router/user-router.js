9080a0f09d0533b7521bfc31a88bfd98daaa999b
himangshumahato82 <himangshumahato82@gmail.com>
2023-05-19 11:56:12 +0530

    update

--- Server/router/user-router.js
+++ Server/router/user-router.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const { REGISTER_USER, LOGIN_USER } = require("../controllers/user-controller");
 const upload = require("../middleware/multer-middleware");
-
+const Authorization = require("../middleware/Authorization-middleware");
+const {
+  Course_Controller
+  
+} = require("../controllers/course-controller");
 const router = express.Router();
 // POST || REGISTER USER
 router.post("/register-user", upload.single("userImage"), REGISTER_USER);
 // POST || LOGIN USER
 router.post("/login-user", LOGIN_USER);
+// GET || GET COURSE
+router.get("/course", Authorization, Course_Controller);
 
 module.exports = router;
